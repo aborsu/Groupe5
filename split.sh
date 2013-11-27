bc071f505276dc254a4b242ded1fd68234f9d821
theyate <sarah.beniamine@gmail.com>
2013-11-27 11:54:12 +0100

    split finish

--- split.sh
+++ split.sh
@@ -1,4 +1,4 @@
 nlines=$(wc -l ftb6_2.mrg | cut -f 1 | grep -P "[0-9]* " -o)
 let "nlines = (nlines/10)*9"
 split ftb6_2.mrg -l $nlines ftb_ -d
-
+ cat ftb_01|tr "\n" "@"|grep -o -P " [^)^(]+\)+@?"|grep -o -P "[^)]+"|tr "\n" " "|sed 's/ @  /\n/g'
