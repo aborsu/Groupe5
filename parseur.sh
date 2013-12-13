08da60707103367e6a2ca7518fbce0b667e63022
theyate <sarah.beniamine@gmail.com>
2013-12-13 18:40:30 +0100

    deux scripts

--- parseur.sh
+++ parseur.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 # Script principal
-# Lance l'extraction de la grammaire et du corpus (main.py)
-# Entraîne un parseur externe (SYNTAX) sur la grammaire et le lexique extrait
-# Lance ledit parser sur un fichier pré-traité
+# Lance un parseur ftb.lex.out situé dans sxpcfg sur un input donné en argument.
 #
 # Argument :
 # 1: chemin vers sxpcfg
@@ -15,13 +13,8 @@
 #   tree.py
 #   parseur.sh (vous êtes ici)
 #   une installation de SYNTAX
-
-python3 main.py -t ftb6_2.mrg -l "lefff_5000.ftb4tags" "utf8" -l "lexique_cmpnd_TP.txt" "latin1"
-cp ftb.lex "$1spec/"
-cp ftb.bnf "$1spec/"
 cp $2 $1
 cd $1
-make ftb.lex.out
 ./ftb.lex.out -n 1 -pnt -string "$2" > output_groupe5.txt
 cd -
 cp "$1output_groupe5.txt" ./
---
+++ train_parser.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# Script principal
+# Lance l'extraction de la grammaire et du corpus (main.py)
+# Entraîne un parseur externe (SYNTAX) sur la grammaire et le lexique extrait
+#
+# Argument :
+# 1: chemin vers sxpcfg
+#
+# Fichiers devant être présents:
+#   main.py
+#   extraction.py
+#   utils.py
+#   tree.py
+#   parseur.sh (vous êtes ici)
+#   une installation de SYNTAX
+
+python3 main.py -t ftb6_2.mrg -l "lefff_5000.ftb4tags" "utf8" -l "lexique_cmpnd_TP.txt" "latin1"
+cp ftb.lex "$1spec/"
+cp ftb.bnf "$1spec/"
+cd $1
+make ftb.lex.out
+
+
