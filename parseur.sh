1ce3682af7be9e611a002f09926bdf801cb11374
theyate <sarah.beniamine@gmail.com>
2013-12-13 17:55:58 +0100

    parseur.sh

--- parseur.sh
+++ parseur.sh
@@ -4,13 +4,17 @@
 # Entraîne un parseur externe (SYNTAX) sur la grammaire et le lexique extrait
 # Lance ledit parser sur un fichier pré-traité
 #
+# Argument : chemin vers spec
+#
 # Fichiers devant être présents:
 #   main.py
 #   extraction.py
 #   utils.py
 #   parseur.sh (vous êtes ici)
 #   une installation de SYNTAX
-
 python3 main.py -t ftb6_2.mrg -l "lefff_5000.ftb4tags" "utf8" -l "lexique_cmpnd_TP.txt" "latin1"
-SYNTAX machin
-machin -n 1 -pnt -string
+cp ftb.lex $1
+cp ftb.bnf $1
+cd $1
+make foo.lex.out 
+?????? -n 1 -pnt -string
