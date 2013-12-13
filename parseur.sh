fd03c12cea5e0a1e2f10f162821c6b8b4267d840
theyate <sarah.beniamine@gmail.com>
2013-12-13 18:19:37 +0100

    script

--- parseur.sh
+++ parseur.sh
@@ -4,7 +4,9 @@
 # Entraîne un parseur externe (SYNTAX) sur la grammaire et le lexique extrait
 # Lance ledit parser sur un fichier pré-traité
 #
-# Argument : chemin vers spec
+# Argument :
+# 1: chemin vers sxpcfg
+# 2: input
 #
 # Fichiers devant être présents:
 #   main.py
@@ -15,8 +17,9 @@
 #   une installation de SYNTAX
 
 python3 main.py -t ftb6_2.mrg -l "lefff_5000.ftb4tags" "utf8" -l "lexique_cmpnd_TP.txt" "latin1"
-cp ftb.lex $1
-cp ftb.bnf $1
-cd $1
-make foo.lex.out 
-?????? -n 1 -pnt -string
+cp ftb.lex "$1/spec/"
+cp ftb.bnf "$1/spec/"
+cp $2 "$1/spec/"
+make "$1/ftb.lex.out"
+cp "$1/.ftb.lex.out" ./
+./ftb.lex.out -n 1 -pnt -string > output_groupe5.txt
